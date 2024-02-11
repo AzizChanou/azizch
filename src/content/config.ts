ec3fc9232735a14da4ead2f3bcca65dd40a8e967
KYOGRE <azizchanou@gmail.com>
2024-02-11 01:35:24 +0100

    UPDATE

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
 	schema: z.object({
+		id: z.number(),
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
