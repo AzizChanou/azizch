c99d0003a80f775452253816d9d010b8cadd83b3
KYOGRE <azizchanou@gmail.com>
2024-02-18 02:00:31 +0100

    UPDATE PORTFOLIO

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://azizch.vercel.app',
   integrations: [mdx(), sitemap(), tailwind(), icon()]
 });
