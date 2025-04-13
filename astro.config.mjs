60ff31377238690e1af95c7239761e1dbfd1ff88
KYOGRE <azizchanou@gmail.com>
2025-04-13 16:39:47 +0100

    Update site URL in astro.config.mjs and add packageManager field in package.json; refactor Default.astro layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://azizch.vercel.app',
+  site: 'https://kyogre.dev',
   integrations: [mdx(), sitemap(), tailwind(), icon()],
   markdown: {
     shikiConfig: {
