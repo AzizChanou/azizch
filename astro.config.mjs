d2c14164c24155a686a5361c231aed05800d14f2
KYOGRE <azizchanou@gmail.com>
2025-07-06 22:25:55 +0100

    feat: update global styles and remove Tailwind config
    - Added custom CSS variables for fonts and animations in global.css.
    - Implemented a wiggle animation using keyframes.
    - Updated layout components to use CSS variables for responsive breakpoints.
    - Removed the Tailwind CSS configuration file as styles are now managed via CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
+import tailwindcss from '@tailwindcss/vite';
 // import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kyogre.dev',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  integrations: [mdx(), sitemap(), icon()],
+
   markdown: {
     shikiConfig: {
       theme: 'dracula',
       wrap: true,
       transformers: [],
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
