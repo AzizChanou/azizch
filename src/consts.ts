24134e59619c51d7da7b2f3dfa37b91fc64feb17
KYOGRE <azizchanou@gmail.com>
2025-07-23 23:54:07 +0100

    Updates default SEO image
    Updates the default SEO image to use the hero image.
    
    This provides a more visually appealing and consistent representation of the site when shared on social media.
    
    Removes unused images.

--- src/consts.ts
+++ src/consts.ts
@@ -6,5 +6,7 @@ export const SITE_URL = "https://azizch.vercel.app";
 
 export const SITE_ICON = "/img/logo_white.svg";
 
+export const SITE_IMAGE = "/img/hero.svg";
+
 export const SITE_DESCRIPTION =
   "Aziz Chanou est un développeur web et mobile";
