96a9cd81fedb8a4689f65e159d128fb5c08e95a8
KYOGRE <azizchanou@gmail.com>
2025-10-14 01:06:07 +0100

    MASS UPDATE

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		id: z.number(),
-		title: z.string(),
-		tags: z.array(z.string()).optional(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		heroImage: z.string(),
-		draft: z.boolean().default(true),
-	}),
+  type: "content",
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    tags: z.array(z.string()).optional(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    heroImage: z.string(),
+    draft: z.boolean().default(true),
+  }),
 });
 
 const projects = defineCollection({
@@ -25,4 +25,12 @@ const projects = defineCollection({
   }),
 });
 
-export const collections = { blog, projects };
+const we = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+});
+
+export const collections = { blog, projects, we };
